5f0bb2fb6d62b073b824c1a09c77164349d27227
Ola Krassowska <ola.krassowska@hotmail.com>
2024-10-22 23:23:20 +0200

    added main.css file and connected with base template

--- TrackMe/settings.py
+++ TrackMe/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = ('static',)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
