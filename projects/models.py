ba8eefdc4c185b44b65052540ce9ca6bc8436dfe
Ola Krassowska <ola.krassowska@hotmail.com>
2024-10-27 20:35:30 +0100

    Removed autocomplete for input, added new icons, added new view for project list template, styling detail view etc

--- projects/models.py
+++ projects/models.py
@@ -8,6 +8,13 @@ class Project(models.Model):
     team = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='projects', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50, choices=[
+        ('To Do', 'To Do'),
+        ('In Progress', 'In Progress'),
+        ('On Hold', 'On Hold'),
+        ('Completed', 'Completed'),
+        ('Canceled', 'Canceled')
+    ], default='To Do')
 
     def __str__(self):
         return self.name
