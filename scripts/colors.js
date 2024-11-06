9362803f1d8b8a3abd6f5755400a0073f82257ac
Ola Krassowska <ola.krassowska@hotmail.com>
2024-11-06 18:31:19 +0100

    colors corrected v2, added palettes in colors.js, migrated from style to js coloring

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,13 +1,19 @@
 const PALETTES = {
     tasks: {
-        "completed": { backgroundColor: "rgb(100,200,100)" },
-        "in_progress": { backgroundColor: "rgb(250, 220, 100)" },
-        "to_do": { backgroundColor: "rgb(250,160,70)" },
+        "completed": {backgroundColor: "rgb(100,200,100)"},
+        "in progress": {backgroundColor: "rgb(250, 220, 100)"},
+        "to do": {backgroundColor: "rgb(250,160,70)"},
+    },
+    projects: {
+        "completed": {backgroundColor: "rgb(100,200,100)"},
+        "in progress": {backgroundColor: "rgb(250, 220, 100)"},
+        "to do": {backgroundColor: "rgb(250,160,70)"},
     },
     notifications: {
-        "accepted": { backgroundColor: "rgb(100,200,100)" },
-        "declined": { backgroundColor: "rgb(200, 100, 100)" },
-        "blocked": { backgroundColor: "rgb(50, 50, 50)", color: "white" },
+        "accepted": {backgroundColor: "rgb(100,200,100)"},
+        "requested" : {backgroundColor: "rgb(250, 220, 100)"},
+        "declined": {backgroundColor: "rgb(200, 100, 100)"},
+        "blocked": {backgroundColor: "rgb(50, 50, 50)", color: "white"},
     }
 };
 
@@ -16,7 +22,7 @@ function applyPaletteStyles() {
 
     elements.forEach(element => {
         let paletteName = element.getAttribute("data-palet");
-        let type = element.getAttribute("data-palet-type");
+        let type = element.getAttribute("data-palet-type").toLowerCase();
 
         if (PALETTES[paletteName] && PALETTES[paletteName][type]) {
             const styles = PALETTES[paletteName][type];
